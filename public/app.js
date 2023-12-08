394303587790834abc60fa77922a5e6a77e7ad50
Termina7or2000 <jaywong8288@gmail.com>
2023-12-08 00:47:59 -0800

    Initial commit

---
+++ public/app.js
@@ -0,0 +1,7 @@
+// You can add JavaScript functionality here if needed
+// For example, you can use JavaScript to dynamically update the content of your portfolio.
+
+// Example: Changing the color of the header on click
+document.querySelector('header').addEventListener('click', function() {
+    this.style.backgroundColor = 'red';
+});
